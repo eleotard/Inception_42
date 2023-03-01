146b6ec933ba0c0bf880c67bd1fb2561d68c4b1f
Elsie Leotard <eleotard@base32>
2023-03-01 01:53:26 +0100

    en fait ca va mieux, ya mariadb et nginx pas mal faits, mais un pb de socket pour maiadb

---
+++ srcs/requirements/mariadb/tools/script.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+
+if [ -d /run/mysql ]; then
+	chown -R mysql:mysql /run/mysql
+else
+	mkdir -p /run/mysql
+	chown -R mysql:mysql /run/mysql
+fi
+		
+
+if [ -d /var/lib/mysql/mysql ]; then
+	chown -R mysql:mysql var/lib/mysql
+
+else
+	mysql_install_db --user=mysql --ldata=var/lib/mysql > /dev/null
+fi
+
+#USE mysql
+
+
+rc-service mariadb start
+
+#Create database
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DDB_NAME}\`;"
+
+#Create user and grant privileges
+#We delete the 'anonymous user' so you can't connect with no username
+#the '%' allows remote connection to all hosts and localhost only from the ddb machine
+mysql -e "DELETE FROM mysql.user WHERE User=''"
+mysql -e "CREATE USER \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DDB_NAME}\`.* TO \`${MYSQL_USER}\`@'%';"
+mysql -e "ALTER USE 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+
+#We apply our changement before changing root password, otherwise the script won't be able to exec the remaining commands
+mysql -e "FLUSH PRIVILEGES;"
+
+
+#Then we change root password
+mysqladmin -u root password $MYSQL_ROOT_PASSWORD shutdown;
+
+exec /usr/bin/mysql --user=mysql --console --skip-name-resolve --skip-networking=0
+
